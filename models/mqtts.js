df1990d1df1335cf27fa7fdfe6fbc498ea50f19f
HJ <skwo777@hufs.ac.kr>
2022-12-12 14:51:38 +0900

    Modify Emergency detect

--- models/mqtts.js
+++ models/mqtts.js
@@ -32,37 +32,44 @@ client.on('connect', () => {
 
 client.on('message', async (myEdukit, message) => {
   const obj = JSON.parse(message.toString());
-  // console.log('her1e');
+
+  // // 비상정지 시작시
+  if (obj.Wrapper[27].value === false && emergencyDetector === false) {
+    emergencyDetector = true;
+    setTimeout(() => {
+      if (obj.Wrapper[27].value === false) {
+        if (prossecing === true) {
+          console.log('작업중 비상정지의 경우');
+        } else {
+          console.log('작업중이 아닌 비상정지의 경우');
+        }
+        console.log('비상정지 기록시작시점');
+        emergencyDetector = true;
+      }
+    }, 1000);
+  }
 
   // 비상정지 종료시
 
   if (obj.Wrapper[27].value === true && emergencyDetector === true) {
-    // 비상정지에 의한 작업종료시 해당작업의 비상정지해제 시간 분기점 필요
-    if (prossecing === true) {
-      console.log('작업중 비상정지의 경우');
-    } else {
-      console.log('작업중이 아닌 비상정지의 경우');
-    }
-    console.log('비상정지 기록정지시점');
-    emergencyDetector = false;
-    prossecing = false;
+    setTimeout(() => {
+      if (emergencyDetector === true) {
+        if (prossecing === true) {
+          console.log('작업중 비상정지의 경우');
+        } else {
+          console.log('작업중이 아닌 비상정지의 경우');
+        }
+        console.log('비상정지 기록정지시점');
+        emergencyDetector = false;
+        prossecing = false;
+      }
+    }, 1000);
   }
 
-  // // 비상정지 시작시
-  if (obj.Wrapper[27].value === false && emergencyDetector === false) {
-    if (prossecing === true) {
-      console.log('작업중 비상정지의 경우');
-    } else {
-      console.log('작업중이 아닌 비상정지의 경우');
-    }
-    console.log('비상정지 기록시작시점');
-    emergencyDetector = true;
-  }
   // 작업 시작 여부 변화 감지 (작업 시작시 한번만 실행)
 
   if (changeDetector !== obj.Wrapper[6].value && obj.Wrapper[6].value === true) {
     changeDetector = obj.Wrapper[6].value;
-    console.log('here333');
     const testMake = await Edukit.create({
       eStop: 'here',
       pdStartTime: Date.now(),
